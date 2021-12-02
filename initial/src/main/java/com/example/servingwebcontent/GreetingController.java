4cf852677012ab293511e20400b99e9f576c183d
wuhepeng <1845664903@qq.com>
2021-12-02 20:38:41 +0800

    添加注解@Controller

--- initial/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ initial/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,6 +1,7 @@
 package com.example.servingwebcontent;
 
 import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @Create 2021-11-16 20:35
  * @Blog https://www.cnblogs.com/WLCYSYS/
  **/
-
+@Controller
 public class GreetingController {
 
     /**
--- initial/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ initial/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -3,6 +3,13 @@ package com.example.servingwebcontent;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @Description: Serving Web Content with Spring MVC
+ * @Author: XiaoShuMu
+ * @Date: 2021/12/2
+ * @Version 1.0
+ * @Blog https://www.cnblogs.com/WLCYSYS/
+ */
 @SpringBootApplication
 public class ServingWebContentApplication {
 
